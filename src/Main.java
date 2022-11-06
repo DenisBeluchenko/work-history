b854ec899bc58db7ba128e0fbe44192db5a1322f
Denis Beluchenko <dzibis@gmail.com>
2022-11-06 19:08:20 +0700

    v1

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] sales={1,5,9,4};
+        SalesManager sale= new SalesManager(sales);
+        System.out.println(sale.max());
+    }
+}
